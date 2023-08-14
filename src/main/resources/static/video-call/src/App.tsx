feb27583259cecfeb552b3af7d355520200ecfa5
Alex Poma <alex.rpoma@outlook.com>
2023-08-14 16:19:34 -0400

    frontend - react

---
+++ src/main/resources/static/video-call/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Login } from './components/login'
+
+function App() {
+
+  return (
+    <>
+      <h2>Welcome to Video call!!</h2>
+      <Login />
+    </>
+  )
+}
+
+export default App
