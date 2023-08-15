137dc6a417d531447c43777245c44c8306aad8cd
Alex Poma <alex.rpoma@outlook.com>
2023-08-15 18:35:53 -0400

    handler values

--- src/main/resources/static/video-call-frontend/src/components/login.tsx
+++ src/main/resources/static/video-call-frontend/src/components/login.tsx
@@ -1,6 +1,26 @@
+import { useState } from 'react'
 import '../styles/login.css'
 
 export const Login: React.FC = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const emailHandleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const email = e.target.value
+    setEmail(email)
+  }
+
+  const passwordHandleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const pass = e.target.value
+    setPassword(pass)
+  }
+
+  const onSubmit = (e: React.FormEvent<HTMLElement>): void => {
+    e.preventDefault()
+    console.log('Email: ', email)
+    console.log('Password', password)
+  }
+
   return (
     <div className='container'>
       <div className='box'>
@@ -9,14 +29,14 @@ export const Login: React.FC = () => {
             <h2>Hello, Again!</h2>
             <small>We are happy to have you back!</small>
           </div>
-          <form className='form'>
+          <form className='form' onSubmit={onSubmit}>
             <div className="input-field">
               <label htmlFor="email">Email:</label>
-              <input className='input-box' id="email" type="email" name="email" />
+              <input className='input-box' id="email" type="email" name="email" onChange={emailHandleChange} />
             </div>
             <div className="input-field">
               <label htmlFor="pass">Password:</label>
-              <input className='input-box' id="pass" type="password" name="password" />
+              <input className='input-box' id="pass" type="password" name="password" onChange={passwordHandleChange} />
               <i className='bx bxs-low-vision'></i>
             </div>
             <div className="remember">
