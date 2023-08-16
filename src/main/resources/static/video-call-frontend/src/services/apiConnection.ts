f96e58cc9583de8798f03cdcabaad66e6fc9beb5
Alex Poma <alex.rpoma@outlook.com>
2023-08-16 16:43:52 -0400

    login error case added.

--- src/main/resources/static/video-call-frontend/src/services/apiConnection.ts
+++ src/main/resources/static/video-call-frontend/src/services/apiConnection.ts
@@ -2,12 +2,16 @@ import { LOGIN_API_URL } from '../constants/apiConnection'
 import type { UserLoginData, User } from '../interfaces/user'
 
 export const login = async (signIn: UserLoginData): Promise<User> => {
-  const user = await fetch(LOGIN_API_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(signIn)
-  })
-  return await user.json()
+  try {
+    const user = await fetch(LOGIN_API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(signIn)
+    })
+    return await user.json()
+  } catch (error) {
+    throw new Error('Fetching error!')
+  }
 }
